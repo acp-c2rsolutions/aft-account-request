811b8acf210e82b2b0141306920d5eefb76ff7eb
Mithun Cheriyath <mithun@cheriyath.in>
2022-03-16 08:06:46 +0530

    Commit a new account

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -5,19 +5,19 @@ module "sandbox_account_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "c2r.acp.aftsandbox.01@gmail.com"
+    AccountEmail = "acp.c2r.aftsandbox.01@gmail.com"
     AccountName  = "sandbox-account-01"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Sandbox"
+    ManagedOrganizationalUnit = "Sanbox"
     # Syntax for nested OU
-    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "c2r.acp.aftsandbox.01@gmail.com"
-    SSOUserFirstName = "c2racp"
+    # ManagedOrganizationalUnit = "Sanbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "acp.c2r.aftsandbox.01@gmail.com"
+    SSOUserFirstName = "acpc2r"
     SSOUserLastName  = "sandbox01"
   }
 
   account_tags = {
-    "ABC:Owner"       = "c2r.acp.aftsandbox.01"
+    "ABC:Owner"       = "acp.c2r.aftsandbox.01"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
@@ -28,7 +28,7 @@ module "sandbox_account_01" {
   }
 
   change_management_parameters = {
-    change_requested_by = "c2r.acp.aftsandbox.01@gmail.com"
+    change_requested_by = "acp.c2r.aftsandbox.01@gmail.com"
     change_reason       = "testing the account vending process"
   }
 
